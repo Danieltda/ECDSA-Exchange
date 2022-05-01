b295d9660fd98514f7fc2cbc0454d4debc863eb8
Daniel <danieltendamme@gmail.com>
2022-05-01 17:01:43 +0200

    add the balances

--- server/index.js
+++ server/index.js
@@ -9,26 +9,41 @@ const secp = require("@noble/secp256k1")
 app.use(cors());
 app.use(express.json());
 
-let privateKeyOne = secp.utils.randomPrivateKey();
+let publicKeys = [];
+let privateKeys = [];
+let publicAndPrivateKey = [];
+
+for (let i = 0 ; i < 3 ; i++){
+  let privateKeyOne = secp.utils.randomPrivateKey();
 
 privateKeyOne  = Buffer.from(privateKeyOne).toString("hex");
 
-console.log(privateKeyOne);
+privateKeys.push(privateKeyOne);
 
 let publicKeyOne = secp.getPublicKey(privateKeyOne);
 
 publicKeyOne = Buffer.from(publicKeyOne).toString("hex");
 
-publicKeyOne = "0x" + publicKeyOne.slice(publicKeyOne.length - 40);
+publicKeys.push("0x" + publicKeyOne.slice(publicKeyOne.length - 40));
 
-console.log(publicKeyOne)
+}
 
+console.log(publicKeys);
+console.log(privateKeys);
+
+for (let i  =0 ; i < publicKeys.length; i ++){
+  publicAndPrivateKey.push(publicKeys[i] + ":" + privateKeys[i])
+}
+
+console.log(publicAndPrivateKey);
 
 const balances = {
-  [publicKeyOne]: 100,
-  "2": 50,
-  "3": 75,
+  [publicKeys[0]]: 100,
+  [publicKeys[1]]: 50,
+  [publicKeys[2]]: 75,
 }
+
+// console.log(balances);
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
