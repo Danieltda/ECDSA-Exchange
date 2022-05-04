42f6b14b2f804dcdb64d667adfec3a4c3d5f90bc
Daniel <danieltendamme@gmail.com>
2022-05-04 08:08:54 +0200

    changes

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
 import "./index.scss";
 const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC("secp256k1");
 
 
 const server = "http://localhost:3042";
@@ -19,18 +21,27 @@ document.getElementById("exchange-address").addEventListener('input', ({ target:
 });
 
 document.getElementById("transfer-amount").addEventListener('click', () => {
-  const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
   const privatekey = document.getElementById("privatekey").value;
 
+const transaction = {amount, recipient} 
+
+const key = ec.keyFromPrivate(privatekey, "hex");
+
+const publicKey = key.getPublic().encode("hex");
+
+const signature = key.sign(SHA256(JSON.stringify(transaction).toString())); 
+
+console.log(signature);
+
   const body = JSON.stringify({
-    sender, amount, recipient, privatekey
+    transaction , signature, publicKey
   });
 
-  const msg = SHA256(body);
 
-  console.log(msg)
+
+  
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
 
--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 3042;
-const secp = require("@noble/secp256k1")
-// const EC = require('elliptic').ec;
+const EC = require('elliptic').ec;
+const ec = new EC("secp256k1");
 const SHA256 = require('crypto-js/sha256');
+
 
 // const ec = new EC('secp256k1');
 
@@ -13,55 +14,79 @@ const SHA256 = require('crypto-js/sha256');
 app.use(cors());
 app.use(express.json());
 
-let publicKeys = [];
-let privateKeys = [];
-let publicAndPrivateKey = [];
+const balances = {};
 
-for (let i = 0 ; i < 3 ; i++){
-  let privateKeyOne = secp.utils.randomPrivateKey();
+for ( let i = 0; i < 3 ; i++){
+  const key = ec.genKeyPair();
+  // console.log(key);
+  const publicKey = key.getPublic().encode("hex");
 
-privateKeyOne  = Buffer.from(privateKeyOne).toString("hex");
+  balances[publicKey] = 100;
 
-privateKeys.push(privateKeyOne);
-
-let publicKeyOne = secp.getPublicKey(privateKeyOne);
-
-publicKeyOne = Buffer.from(publicKeyOne).toString("hex");
-
-publicKeys.push("0x" + publicKeyOne.slice(publicKeyOne.length - 40));
-
+  console.log(`Public key --> ${publicKey} --> Private key --> ${key.getPrivate().toString(16)}`)
 }
 
-console.log(publicKeys);
-console.log(privateKeys);
+console.log(balances);
 
-for (let i  =0 ; i < publicKeys.length; i ++){
-  publicAndPrivateKey.push(publicKeys[i] + ":" + privateKeys[i])
-}
+// let publicKeys = [];
+// let privateKeys = [];
+// let publicAndPrivateKey = [];
 
-console.log(publicAndPrivateKey);
+// for (let i = 0 ; i < 3 ; i++){
+//   let privateKeyOne = secp.utils.randomPrivateKey();
 
-const balances = {
-  [publicKeys[0]]: 100,
-  [publicKeys[1]]: 50,
-  [publicKeys[2]]: 75,
-}
+// privateKeyOne  = Buffer.from(privateKeyOne).toString("hex");
+
+// privateKeys.push(privateKeyOne);
+
+// let publicKeyOne = secp.getPublicKey(privateKeyOne);
+
+// publicKeyOne = Buffer.from(publicKeyOne).toString("hex");
+
+// publicKeys.push("0x" + publicKeyOne.slice(publicKeyOne.length - 40));
+
+// }
+
+// console.log(publicKeys);
+// console.log(privateKeys);
+
+// for (let i  =0 ; i < publicKeys.length; i ++){
+//   publicAndPrivateKey.push(publicKeys[i] + ":" + privateKeys[i])
+// }
+
+// console.log(publicAndPrivateKey);
+
+// const balances = {
+//   [publicKeys[0]]: 100,
+//   [publicKeys[1]]: 50,
+//   [publicKeys[2]]: 75,
+// }
 
 // console.log(balances);
 
 app.get('/balance/:address', (req, res) => {
   const {address} = req.params;
-  const balance = balances[address] || 0;
+  const balance = balances[address.toLocaleLowerCase()] || 0;
   res.send({ balance });
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount, privatekey} = req.body;
-  console.log(req.body)
-  console.log(privatekey)
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender], privatekey   });
+  const {transaction, signature, publicKey} = req.body;
+  console.log("watch" + transaction);
+  console.log(signature);
+
+  const key = ec.keyFromPublic(publicKey, "hex");
+
+  const hash = SHA256(JSON.stringify(transaction).toString()); 
+
+  if(key.verify(hash, signature)){
+    balances[publicKey] -= transaction.amount;
+    balances[transaction.recipient] = (balances[transaction.recipient] || 0) + +transaction.amount;
+    res.send({ balance: balances[publicKey]});
+  } else{
+    res.sendStatus(400);
+  }
+ 
 });
 
 app.listen(port, () => {
